e4fa2e7e9d64fd754f0b5d308a60ced0cade36f2
priyadarshi6425 <137717934+priyadarshi6425@users.noreply.github.com>
2023-08-25 17:15:40 +0530

    Update resource_grp.tf

--- resource_grp.tf
+++ resource_grp.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "app_grp" {
-  name     = "app-grp6"
+  name     = "app-grp7"
   location = "North Europe"
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "${terraform.workspace}-1"
+  name                = "${terraform.workspace}-2"
   location            = azurerm_resource_group.app_grp.location
   resource_group_name = azurerm_resource_group.app_grp.name
   address_space       = ["10.0.0.0/16"]
